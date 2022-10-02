03c3f67260cba14aa4f16ffdd3033b0f8cb809c9
monhi <m.monhi@gmail.com>
2022-10-02 13:05:46 +0330

    First version

---
+++ README.md
@@ -0,0 +1,7 @@
+# Profit Rate Calculator 
+
+This is a simple html page to calculate the total profit rate after defined number of iterations and based on a constant profit rate.
+
+As I use this a lot for crypto market in different places, I decided to make a repo for it.
+
+In this way I can download it wherever I want.
---
+++ js/rate.js
@@ -0,0 +1,24 @@
+window.onload=function()
+{
+	document.querySelector("#init_invest").value = "1.0";
+	document.querySelector("#profit_rate").value = "0.035";
+	document.querySelector("#iteration").value = "52"; // 52 weeks in a year.
+
+}
+
+function CalcProfitRate()
+{
+	let invest 		= parseFloat(document.querySelector("#init_invest").value);
+	let rate   		= parseFloat(document.querySelector("#profit_rate").value);
+	let iteration	= parseInt(document.querySelector("#iteration").value,10);
+	let result = "";
+	let ftemp = 1.0;
+	
+	for (let i=0; i <iteration; i++)
+	{
+		invest += invest*rate;
+		ftemp = Math.round(invest*10000)/10000;
+		result += `${i+1}th iter: ${ftemp}\n`;
+	}
+	document.querySelector("#result").innerHTML = result;
+}
